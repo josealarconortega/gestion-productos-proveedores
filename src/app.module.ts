e3dd7b944bab5073db586199efcbe79a529c82e7
josealarconortega <felipe_alarcon@hotmail.cl>
2024-06-06 02:46:00 -0400

    Se incorpora login y crud de usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+    isGlobal: true, // hace que el modulo de configuración sea global, es accesible en cualquier módulo sin necesidad de importalo nuevamente
+  }),
+  MongooseModule.forRoot('mongodb://localhost/db-social-network'),
+  UsersModule,
+  AuthModule,],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
